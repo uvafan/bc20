3511f1cc1953412b1466944da6821e07588c9b14
uvafan <eli.d.lifland@gmail.com>
2020-01-16 09:20:43 -0500

    landscaper bug fix

--- src/v2_better_turtle/NetGun.java
+++ src/v2_better_turtle/NetGun.java
@@ -1,4 +1,4 @@
-package v2;
+package v2_better_turtle;
 
 import battlecode.common.GameActionException;
 import battlecode.common.RobotController;
--- src/v2_better_turtle/Utils.java
+++ src/v2_better_turtle/Utils.java
@@ -1,4 +1,4 @@
-package v2;
+package v2_better_turtle;
 
 import battlecode.common.Clock;
 
