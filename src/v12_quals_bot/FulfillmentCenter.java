850721fe25af834620e392458da80abed18ae9a7
jambyz <jambyz@gmail.com>
2020-01-29 10:58:11 -0500

    v12 quals bot
    it's back

---
+++ src/v12_quals_bot/FulfillmentCenter.java
@@ -0,0 +1,31 @@
+package v12_quals_bot;
+
+import battlecode.common.*;
+
+public class FulfillmentCenter extends Building {
+
+    public static MapLocation minerLoc = null;
+
+    public FulfillmentCenter(RobotController r) throws GameActionException {
+        super(r);
+        RobotInfo[] friends = rc.senseNearbyRobots(2,us);
+        for(RobotInfo f: friends) {
+            if(f.type == RobotType.MINER) {
+                minerLoc = f.location;
+                break;
+            }
+        }
+    }
+
+    public void takeTurn() throws GameActionException {
+        super.takeTurn();
+        comms.readMessages();
+    }
+
+    @Override
+    public Direction getBuildDirection() {
+        if(hqLoc != null && hqAttacked)
+            return here.directionTo(hqLoc);
+        return hqLoc.directionTo(here);
+    }
+}
